128c1f458831efd89e347a80153ec3c884c7b876
m.AnimeRTR <subanime956@gmail.com>
2026-04-15 01:05:29 -0500

    Delete assets/js/subir.js

--- assets/js/subir.js
+++
@@ -1,9 +0,0 @@
-  const btn = document.getElementById("scrollToTopBtn");
-
-  window.addEventListener("scroll", () => {
-    btn.style.display = (window.scrollY > 200) ? "block" : "none";
-  });
-
-  btn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });

638a2aa8cf63878f287c41bdafefe6dd031f44f5
m.AnimeRTR <subanime956@gmail.com>
2026-05-03 14:21:38 -0500

    Delete assets/js directory

--- assets/js/Onesignalpush.js
+++
@@ -1,6 +0,0 @@
-  window.OneSignalDeferred = window.OneSignalDeferred || [];
-  OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-      appId: "4d0acb4d-9553-4462-860a-0fe733f174c9",
-    });
-  });

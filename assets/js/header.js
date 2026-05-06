645ed6b6da81706127dc1b2a426e7453748f8a26
m.AnimeRTR <subanime956@gmail.com>
2026-05-06 01:33:08 -0500

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -260,21 +260,3 @@ OneSignalDeferred.push(async function (OneSignal) {
 
 
 
-// 🔒 activar bloqueo apenas carga el script
-document.body.classList.add("loading");
-
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-
-  if (loader) {
-    loader.style.transition = "opacity .3s ease";
-    loader.style.opacity = "0";
- 
-    setTimeout(() => {
-      loader.remove();
-
-      // 🔓 liberar scroll
-      document.body.classList.remove("loading");
-    }, 300);
-  }
-});

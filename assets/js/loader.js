0526bc373f2a04ebd614baf3dbcadc53082de594
m.AnimeRTR <subanime956@gmail.com>
2026-05-06 01:34:31 -0500

    Create loader.js

---
+++ assets/js/loader.js
@@ -0,0 +1,19 @@
+
+// 🔒 activar bloqueo apenas carga el script
+document.body.classList.add("loading");
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  if (loader) {
+    loader.style.transition = "opacity .3s ease";
+    loader.style.opacity = "0";
+ 
+    setTimeout(() => {
+      loader.remove();
+
+      // 🔓 liberar scroll
+      document.body.classList.remove("loading");
+    }, 300);
+  }
+});
